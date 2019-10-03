3294564f34338e1e7d92a896fe7a235ac91f8159
Max Koreychenko <MKoreychenko@korusconsulting.ru>
2019-10-03 18:08:55 +0300

    Remove unused CORS middleware

--- html/public/api/index.php
+++ html/public/api/index.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Container\Container;
 use Slim\Factory\AppFactory;
-use Tuupola\Middleware\CorsMiddleware;
 use App\Controllers\Middlewares\JsonBodyParseMiddleware;
 
 require __DIR__ . '/../../vendor/autoload.php';
@@ -12,14 +11,6 @@ require __DIR__ . '/../../app/config/config.php';
 require __DIR__ . '/../../app/config/container.php';
 
 $app = AppFactory::create(null, $container);
-
-$app->add(new CorsMiddleware([
-    "origin" => ["*"],
-    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    "headers.allow" => ["*"],
-    "credentials" => true,
-    "cache" => 86400
-]));
 
 require __DIR__ . '/../../app/config/routing.php';
 
